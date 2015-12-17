6f72c694e9e30fde9f4974312073c3bb3e4d7781
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2015-12-17 18:13:54 -0500

    Use Clang from the NDK, user no longer needs to symlink ld, push libc++

--- release/deploy.sh
+++ release/deploy.sh
@@ -9,12 +9,13 @@ do
 	echo $i
 	strip -o bin/$i ../bin/$i
 done
-for i in swift-autolink-extract swiftc swiftc-android swift-android-push
+for i in swift-autolink-extract swiftc swiftc-android swift-android-push armv7-none-linux-androideabi-ld
 do
 	cp -P ../bin/$i bin/$i
 done
 mkdir lib
 cp -r ../lib/swift lib/
+[ -a lib/swift/android/libicuuc.so ] || echo "WHERE IS ICU"
 cd ..
 rm swift_android.tar.xz
 tar cJf swift_android.tar.xz swiftandroid
