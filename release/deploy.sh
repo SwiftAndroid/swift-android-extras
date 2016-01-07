d65b0a8868b9a2682272bf5a47dd42a3d7e0391e
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2016-01-06 22:29:59 -0500

    add androidfix script to fix path in Bionic module.map, copy swiftpm executables and clang headers in deploy

--- release/deploy.sh
+++ release/deploy.sh
@@ -9,13 +9,18 @@ do
 	echo $i
 	strip -o bin/$i ../bin/$i
 done
-for i in swift-autolink-extract swiftc swiftc-android swift-android-push armv7-none-linux-androideabi-ld
+for i in swift-autolink-extract swiftc swiftc-android swift-android-push armv7-none-linux-androideabi-ld swift-androidfix swiftc-pm-android
 do
 	cp -P ../bin/$i bin/$i
 done
 mkdir lib
 cp -r ../lib/swift lib/
 [ -a lib/swift/android/libicuuc.so ] || echo "WHERE IS ICU"
+cp -r ../../swiftpm-linux-x86_64/lib/swift lib/
+cp -r ../../swiftpm-linux-x86_64/debug/swift-build bin/
+cp -r ../../llbuild-linux-x86_64/bin/* bin/
+rm lib/swift/clang
+cp -r ../../llvm-linux-x86_64/lib/clang/3.8.0 lib/swift/clang
 cd ..
 rm swift_android.tar.xz
 tar cJf swift_android.tar.xz swiftandroid
